1cbb1e2a605df6182cafa8bab06d5b5f86b3cbda
Wan <3354054116@qq.com>
2026-05-14 09:50:54 +0800

    初步完成agent的pom文件和yml文件配置

---
+++ 12306-Component/services/agent-service/src/main/java/com/grace/train12306/biz/agentservice/AgentServiceApplication.java
@@ -0,0 +1,16 @@
+package com.grace.train12306.biz.agentservice;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class AgentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgentServiceApplication.class, args);
+    }
+}
